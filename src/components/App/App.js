5c8ea6934eafde8282365093db1bfb6fa6aacc4f
Alek <alekaker@outlook.com>
2019-09-05 15:05:12 -0600

    async fetch function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      audiobooks: []
+      audiobooks: [],
     }
   }
 
@@ -21,13 +21,25 @@ class App extends Component {
     )
   }
 
-  componentDidMount() {
-    const url = "https://itunes.apple.com/search?term=adventure&media=audiobook"
-    fetch(url)
-    .then(res => res.json())
-    .then(data => this.setState({
-      audiobooks: data.results
-    }))
+
+async getAudio(category) {
+  const url = `https://itunes.apple.com/search?term=${category}&media=audiobook`
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('There was an error getting your data');
+    }
+    const audiobooks = await response.json();
+    return audiobooks.results
+  } catch(error) {
+    throw new Error(error.message);
+  }
+}
+
+
+  async componentDidMount() {
+    const audio = await this.getAudio("Fiction")
+    this.setState({audiobooks: audio})
   }
 
 }
