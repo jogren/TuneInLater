d681ad5d029786da25f526f72ba2b8733b37f771
Jacob Ogren <jogren0044@gmail.com>
2019-09-09 15:23:20 -0600

    Add interweave for card details

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Markup } from 'interweave';
 import './CardDetails.css';
 
 const CardDetails = ({author_name, artwork_url, book_name, description }) => {
@@ -8,7 +9,7 @@ const CardDetails = ({author_name, artwork_url, book_name, description }) => {
       <Link to={'/'} className='back-btn'>◀ back</Link>
       <h4>{book_name}</h4>
       <p>{author_name}</p>
-      <p>{description}</p>
+      <p><Markup content={description}/></p>
       <img src={artwork_url} />
     </section>
   )
