201e7f50656cd08faf7f8ecc16b5274e8e00472f
Alek <alekaker@outlook.com>
2019-09-10 12:41:52 -0600

    fix button fonts

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -41,7 +41,7 @@ class Nav extends Component {
             value={this.state.search}
             onChange={e => this.handleSearch(e)}
             / >
-          <button className="nav-button"onClick={() => newSearch(this.state.search) }><h2>Search</h2></button>
+          <button className="nav-button"onClick={() => newSearch(this.state.search) }><h2>Search..</h2></button>
         </div>
         <select onChange={(e) => newSearch(e.target.value)} className="select-container">
           <option value="">Select Genre:</option>
