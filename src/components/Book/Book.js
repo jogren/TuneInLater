6ca6bdb1b2107d01b8b9291782865b0c465af834
Jacob Ogren <jogren0044@gmail.com>
2019-09-10 11:46:37 -0600

    Add favorite images that toggle

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import headphones from './headphones.svg';
 import headphonesActive from './headphones-active.svg'
+import { connect } from 'react-redux';
 import './Book.css';
 
-const Book = ({ book_name, author_name, artwork_url, book_id, primary_genre_name, release_date, description, toggleFavorite, favorite }) => {
+const Book = ({ book_name, author_name, artwork_url, book_id, primary_genre_name, release_date, description, toggleFavorite, favorite, favoritesReducer }) => {
+  let headphonesImg = favoritesReducer.find(favorite => favorite.book_id === book_id) ? headphonesActive : headphones;
+
+
 
   return(
     <section className="Book_section">
-      <img src={artwork_url} alt={book_name}/>
+      <img src={artwork_url} alt={book_name} className="Book_image"/>
       <h3>{book_name}</h3>
       <p>Author(s): {author_name}</p>
-      <button onClick={() => toggleFavorite({ book_name, author_name, artwork_url, book_id, description, primary_genre_name, release_date, favorite }) } className="Book_button">Star</button>
+      <img src={headphonesImg} onClick={() => toggleFavorite({ book_name, author_name, artwork_url, book_id, description, primary_genre_name, release_date, favorite }) } className="Book_image-favorite" />
       <Link to={`/details/${book_id}`} className="link">
-        <button>See more Info</button>
+        <button className="Book_button">See more Info</button>
       </Link>
     </section>
   )
 }
 
-export default Book;
+const mapStateToProps = state => ({
+  favoritesReducer: state.toggleFavoriteReducer
+})
+
+export default connect(mapStateToProps)(Book);
