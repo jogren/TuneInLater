aa06b0c86a4b785577e3482643b3fc520f929726
Jacob Ogren <jogren0044@gmail.com>
2019-09-10 10:44:36 -0600

    Add headphones svgs

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import headphones from './headphones.svg';
+import headphonesActive from './headphones-active.svg'
 import './Book.css';
 
 const Book = ({ book_name, author_name, artwork_url, book_id, primary_genre_name, release_date, description, toggleFavorite, favorite }) => {
@@ -10,6 +12,8 @@ const Book = ({ book_name, author_name, artwork_url, book_id, primary_genre_name
       <h3>{book_name}</h3>
       <p>{author_name}</p>
       <img src={artwork_url} alt={book_name}/>
+      <img src={headphones}/>
+      <img src={headphonesActive}/>
       <Link to={`/details/${book_id}`} className="link">
         <button>See more Info</button>
       </Link>
